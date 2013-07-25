eedfa2848792ca32809c9638a3775a0d3d025fa4
ascott18 <ascott18@gmail.com>
2013-07-25 14:06:53 -0700

    Added two libraries that were missing.

---
+++ SettingsSerializer/SettingsSerializer.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace AndrewScott.SettingsSerializer
+{
+    /// <summary>
+    /// Represents a set of settings that will be stored and retrieved.
+    /// </summary>
+    [Serializable]
+    public class SettingsData
+    {
+        /// <summary>
+        /// A dictionary that contains objects that will be serialized and deserialized.
+        /// </summary>
+        public Dictionary<string, object> Settings = new Dictionary<string, object>();
+    }
+
+    /// <summary>
+    /// Manages reading, writing, serialization, and deserialization of a set of settings.
+    /// </summary>
+    public class Settings
+    {
+        /// <summary>
+        /// Reference to the <c>SettingsData</c> object that this <c>Settings</c> is handling.
+        /// </summary>
+        public SettingsData Data;
+
+        /// <summary>
+        /// The name and path of the file that will be written to and read from.
+        /// </summary>
+        public readonly string FileName;
+
+        /// <summary>
+        /// Retrieves a setting.
+        /// </summary>
+        /// <param name="key">The key of the requested setting</param>
+        /// <returns>The requested object instance of the setting, or null if it was not found.</returns>
+        public object GetSetting(string key)
+        {
+            if (!IsLoaded)
+                throw new InvalidOperationException("Settings cannot be queried before the settings have been loaded");
+
+            if (Data.Settings.ContainsKey(key))
+                return Data.Settings[key];
+            else
+                return null;
+        }
+
+        /// <summary>
+        /// Sets a setting.
+        /// </summary>
+        /// <param name="key">The key of the setting</param>
+        /// <param name="setting">The value of the setting</param>
+        public void SetSetting(string key, object setting)
+        {
+            if (!IsLoaded)
+                throw new InvalidOperationException("Settings cannot be set before they have been loaded");
+
+            Data.Settings[key] = setting;
+        }
+
+        /// <summary>
+        /// A delegate that will be used for events that communicate data about read/write actions.
+        /// </summary>
+        /// <param name="Data"></param>
+        public delegate void SettingsEventHandler(SettingsData Data);
+
+        /// <summary>
+        /// Event that will be fired when the managed file is read from and loaded into this <c>Settings</c>
+        /// </summary>
+        public event SettingsEventHandler ReadFile;
+
+        /// <summary>
+        /// Event that will be fired when the settings managed by this <c>Settings</c> are written to its file.
+        /// </summary>
+        public event SettingsEventHandler WriteFile;
+
+        /// <summary>
+        /// True if the settings for this <c>Settings</c> have been read from their file since it was created.
+        /// </summary>
+        public bool IsLoaded { get; private set; }
+
+        /// <summary>
+        /// Creates a new <c>Settings</c> to manage a file and a set of data.
+        /// </summary>
+        /// <param name="fileName">The name (and path) of the file that will be read to and written from. </param>
+        /// <param name="readNow"><c>True</c> if the file should be read immediately. 
+        ///     <c>False</c> if the file will be read manually later using <c>Settings.ReadFromFile()</c></param>
+        public Settings(string fileName, bool readNow)
+        {
+            FileName = fileName;
+
+            if (readNow) ReadFromFile();
+        }
+
+
+        /// <summary>
+        /// Writes the settings being managed by this <c>XMLSettings</c> to its file.
+        /// </summary>
+        public void WriteToFile()
+        {
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(FileName, FileMode.Create, FileAccess.Write, FileShare.None);
+            formatter.Serialize(stream, Data);
+            stream.Close();
+
+            if (WriteFile != null) WriteFile(Data);
+        }
+
+        /// <summary>
+        /// Reads the settings being managed by this <c>XMLSettings</c> from its file.
+        /// </summary>
+        public void ReadFromFile()
+        {
+
+            FileInfo fi = new FileInfo(FileName);
+            if (!fi.Directory.Exists)
+                System.IO.Directory.CreateDirectory(fi.DirectoryName);
+
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new FileStream(FileName, FileMode.OpenOrCreate, FileAccess.Read, FileShare.Read);
+
+            if (stream.Length == 0)
+                Data = new SettingsData();
+            else
+                Data = (SettingsData)formatter.Deserialize(stream);
+
+            stream.Close();
+
+            IsLoaded = true;
+
+            if (ReadFile != null) ReadFile(Data);
+        }
+
+        /// <summary>
+        /// Declares a default setting
+        /// </summary>
+        /// <param name="key">The key of the setting as it exists in <c>SettingsData.Settings</c></param>
+        /// <param name="initializer">A delegate that will initialize an instance of the setting class if it doesn't exist.</param>
+        public void DeclareDefault(string key, Func<object> initializer)
+        {
+            if (!IsLoaded)
+                throw new InvalidOperationException("Defaults cannot be declared until the settings have been loaded");
+
+            if (!Data.Settings.ContainsKey(key))
+                Data.Settings[key] = initializer();
+        }
+
+        /// <summary>
+        /// Declares a default setting
+        /// </summary>
+        /// <param name="key">The key of the setting as it exists in <c>SettingsData.Settings</c></param>
+        /// <param name="defaultValue">A value type that represents the default value for the setting in case it doesn't exist in the settings.</param>
+        public void DeclareDefault(string key, ValueType defaultValue)
+        {
+            if (!IsLoaded)
+                throw new InvalidOperationException("Defaults cannot be declared until the settings have been loaded");
+
+            if (!Data.Settings.ContainsKey(key))
+                Data.Settings[key] = defaultValue;
+        }
+
+        /// <summary>
+        /// Declares a default setting
+        /// </summary>
+        /// <param name="key">The key of the setting as it exists in <c>SettingsData.Settings</c></param>
+        /// <param name="defaultValue">A string that represents the default value for the setting in case it doesn't exist in the settings.</param>
+        public void DeclareDefault(string key, string defaultValue)
+        {
+            if (!IsLoaded)
+                throw new InvalidOperationException("Defaults cannot be declared until the settings have been loaded");
+
+            if (!Data.Settings.ContainsKey(key))
+                Data.Settings[key] = defaultValue;
+        }
+
+    }
+}
---
+++ SimpleCommandManager/CommandManager.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AndrewScott.SimpleCommandManager
+{
+    /// <summary>
+    /// A simple class that manages commands and menus for an application.
+    /// To implement, override the <c>ProcessCmdKey</c> method on the <c>System.Windows.Forms.Control</c>
+    /// that you wish to manage commands for using the proper return method specified by
+    /// http://msdn.microsoft.com/en-us/library/system.windows.forms.control.processcmdkey.aspx
+    /// </summary>
+    public class CommandManager : List<Command>
+    {
+        public bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            bool processed = false;
+
+            foreach (Command command in this.Where(c => 
+                (keyData == c.Keys)
+                && (c.Enabled == null || c.Enabled())
+            ))
+            {
+                command.Action();
+
+                processed = true;
+            }
+
+            return processed;
+        }
+    }
+
+    public class Command
+    {
+        public readonly Keys Keys;
+        public readonly ToolStripMenuItem MenuItem;
+        public readonly Action Action;
+
+        private Func<bool> enabled;
+        public Func<bool> Enabled
+        {
+            get { return enabled; }
+            set
+            {
+                if (MenuItem != null)
+                    MenuItem.Paint += MenuItem_Paint_Enabled;
+
+                enabled = value;
+            }
+        }
+
+        void MenuItem_Paint_Enabled(object sender, EventArgs e)
+        {
+            MenuItem.Enabled = Enabled();
+        }
+
+
+        private Func<bool> _checked;
+        public Func<bool> Checked
+        {
+            get { return _checked; }
+            set
+            {
+                if (MenuItem != null)
+                    MenuItem.Paint += MenuItem_Paint_Checked;
+
+                _checked = value;
+            }
+        }
+
+        void MenuItem_Paint_Checked(object sender, EventArgs e)
+        {
+            MenuItem.Checked = Checked();
+        }
+
+
+        public Command(Action action, Keys keys, ToolStripMenuItem menuItem)
+        {
+            Action = action;
+            Keys = keys;
+            MenuItem = menuItem;
+
+            MenuItem.Click += MenuItem_Click;
+        }
+
+        public Command(Action action, Keys keys)
+        {
+            Action = action;
+            Keys = keys;
+        }
+
+        public Command(Action action, ToolStripMenuItem menuItem)
+        {
+            Action = action;
+            MenuItem = menuItem;
+
+            MenuItem.Click += MenuItem_Click;
+        }
+
+
+        void MenuItem_Click(object sender, EventArgs e)
+        {
+            Action();
+        }
+    }
+}
