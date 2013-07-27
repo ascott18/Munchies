9868b1b8b632d87889c7e8d0f7fbd0a2bbca6e35
ascott18 <ascott18@gmail.com>
2013-07-27 00:35:31 -0700

    The main window now uses a proper Melvin icon instead of the default one with the Picasso squares.

--- Munchies/MainWindow.Designer.cs
+++ Munchies/MainWindow.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newGameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -289,6 +290,7 @@
             this.ClientSize = new System.Drawing.Size(640, 504);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.MinimumSize = new System.Drawing.Size(640, 504);
             this.Name = "MainWindow";
