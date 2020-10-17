669b257200e8a6f117d5c42d1b4ec516ac979cfd
Andrew Scott <ascott18@gmail.com>
2020-10-16 18:49:30 -0700

    Fix build, don't set audio on startup because it sets the entire system audio through wasapi.

--- Munchies/MainWindow.cs
+++ Munchies/MainWindow.cs
@@ -407,7 +407,6 @@ namespace Munchies
 
 			// Initialize the audio manager
 			AudioManager.Initialize();
-			AudioManager.Volume = (int)Program.Settings.GetSetting("SoundVolume") / (float)volumeLevelMax;
 			InitializeVolumeMenu();
 		}
 
